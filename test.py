69532655bd6118e1e9ae89ba4bdd46ffa9fb54cf
junaidalam2 <junaid.alam2@gmail.com>
2025-11-07 23:51:55 -0800

    test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ print(doc.paragraphs[0].runs[2].text)
 print(doc.paragraphs[0].runs[3].text)
 
 doc2 = docx.Document()
-doc2.add_paragraph('Hello, World!', style='Heading 1')
+doc2.add_paragraph('Hello, World', style='Heading 1')
 paraObject = doc2.add_paragraph('This is a test paragraph.')
 
 paraObject.add_run(' This is an added run.').bold = True
